9438912e871d4297fcf7d1c7688116641b2d5273
Will Lake <william.j.lake2000@gmail.com>
2021-08-07 18:07:08 +0100

    Update database set up logging

--- src/main/java/com/willlake/ringingapi/methods/data/MethodRepoControl.java
+++ src/main/java/com/willlake/ringingapi/methods/data/MethodRepoControl.java
@@ -1,11 +1,16 @@
 package com.willlake.ringingapi.methods.data;
 
+import com.willlake.ringingapi.RingingApiApplication;
 import com.willlake.ringingapi.databaseObj.Method;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashSet;
 import java.util.Set;
 
 public class MethodRepoControl {
+    private static final Logger log = LoggerFactory.getLogger(MethodRepoControl.class);
+
     private final MethodRepository repository;
     private final ReadMethodXml readMethodXml;
 
@@ -15,8 +20,10 @@ public class MethodRepoControl {
     }
 
     public void addMethodsFromFile() {
+        log.info("Adding Methods to database");
         Set<Method> returnSet = new HashSet<>(readMethodXml.readXml());
-        returnSet.forEach(repository::save);
+        repository.saveAll(returnSet);
+        log.info("Added " + (long) returnSet.size() + " Methods to database");
     }
 
     public void clearAllMethodsFromDB() {
